5681c56e2e5cd8b1e83b0f21600df925e477664d
vaneck237 <vaneckbilongo@gmail.com>
2023-05-04 16:08:58 +0100

    change validityDate

--- src/pickers/ProgramPicker.js
+++ src/pickers/ProgramPicker.js
@@ -31,7 +31,7 @@ const ProgramPicker = (props) => {
                       id
                       idProgram
                       nameProgram
-                      validityDate
+                      validityDateFrom
                       ${extraFragment ?? ""}
                     }
                 }
